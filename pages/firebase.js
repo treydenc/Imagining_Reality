ec48f9a0ffc576d48ceaf6c97d97e2be87a8934c
Treyden <treyden3@gmail.com>
2023-10-25 15:45:49 -0400

    fix

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBWaEb-5-XOCiguHUuCKQFUAkOZwkjaNYQ",
